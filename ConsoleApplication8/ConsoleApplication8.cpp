6421b5933d783bf91ecb59322a85536d096b9ed4
User <User@HOME-PC>
2023-10-07 22:47:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+using namespace std;
+void changing(char* arr) {
+	char* strr;
+	for (strr = arr; *strr; strr++) {
+		if (*strr == '.') {
+			*strr = '!';
+		}
+	}
+	
+}
+
+void cheak(char* arr, int coun) {
+	
+	int count = 0;
+	char* str;
+	for (str = arr; *str; str++) {
+		if (*str == *arr) {
+			count++;
+		}
+		
+	}
+	cout << "amount of same symbols"<< " " << count;
+}
+int main()
+{
+	char ar[20] = "..ar.ar..";
+
+	changing(ar);
+	cout << ar << "\n";
+	char arr[] = "!,@,#,!,%";
+	
+	int count = 0 ;
+	cheak(arr, count);
+	
+	
+	
+	
+	
+}
+
